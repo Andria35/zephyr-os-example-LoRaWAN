aac85908f6754c48290ee653ecaacdeb382ebb1d
Andria Inasaridze <andria.inas@gmail.com>
2026-01-12 16:18:45 +0100

    Ignore build artifacts

--- build/modules/picolibc/newlib/libc/machine/arm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andriasworld/zephyrproject/modules/lib/picolibc/newlib/libc/machine/arm
-# Build directory: /Users/andriasworld/Programming/iot-engineering/zephyr‐os‐example‐LoRaWAN/build/modules/picolibc/newlib/libc/machine/arm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("machine")
